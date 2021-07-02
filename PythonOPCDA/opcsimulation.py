ca9d2d86bc3fac8e14d9a69f500fb4897b6ea0b6
squidmoron <aditferdianto@gmail.com>
2021-07-02 19:33:13 +0700

    Create opcsimulation.py

---
+++ PythonOPCDA/opcsimulation.py
@@ -0,0 +1,69 @@
+#------------------------------------------------------------------------
+#-----Name 		: Aditya Ferdianto       --------------------------------
+#-----Prog 		: Python for Read OPC DA --------------------------------
+#-----Company : Mattel Inc.            --------------------------------
+#-----Date		: June 2021              --------------------------------
+#------------------------------------------------------------------------
+
+import OpenOPC
+import win32pdh
+import Pyro4
+import time
+import datetime
+import pywintypes
+from multiprocessing import Process
+from parsing import parsing
+
+import influxdb
+import json
+from influxdb import InfluxDBClient
+from win10toast import ToastNotifier
+#toaster = ToastNotifier()
+
+#toaster.show_toast("Python for Read OPC DA","Reading OPC DA",duration=25)
+
+#--------------------------Variable All----------------------------------
+host ='Matrikon.OPC.Simulation'
+pywintypes.datetime = pywintypes.TimeType
+opc = OpenOPC.client()
+
+#-----for trial read data opc
+#opc.connect(host)
+#x=opc.read('Random.Real8')
+#print(x)
+
+AVG_CTArm				=''
+AVG_CTFront			=''
+AVG_CTBack			=''
+AVG_CTNeck			=''
+AVG_CTConnector	=''
+
+#------------------------------------------------------------------------
+class dataOPC:
+  def __init__(self, valueOPC, qualityOPC, datetimeOPC):
+    self.valueOPC = valueOPC
+    self.qualityOPC = qualityOPC
+    self.datetimeOPC = datetimeOPC
+#------------------------------------------------------------------------
+def readOPC(variable) :
+	opc.connect(host)
+	try	:
+		value=parsing(str(opc.read(variable)))
+		value=dataOPC(value[0],value[1],value[2])
+		if(value.qualityOPC=="Good"):
+			#print(value.valueOPC)
+			return value
+	except :
+		print("============================= read problem =============================")
+	opc.close()
+#------------------------------------------------------------------------
+
+while 1 :
+	try:
+		AVG_CTArm=readOPC('Random.Real8')
+		print("avg CT: ",str(round(float(AVG_CTArm.valueOPC),2)))
+	except :
+		print("Can not read opc data or data equal to Not Good")
+	time.sleep(.1)
+
+tes()
